db4c6eae15a79ef51fb7001110fc5ecd3e1beb48
Kartikay Dubey <90677227+dubeyKartikay@users.noreply.github.com>
2023-04-03 17:02:04 +0530

    version 1.0

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,7 +1,16 @@
 import styles from "./Footer.module.scss";
-
+import GitHub from "../../public/github.svg"
+import Instagram from "../../public/instagram.svg"
+import Image from "next/image";
 export default function Footer() {
   return (
-    <div className={styles.footer} >Footer</div>
+    <div className={styles.footer} >
+      <a href="https://github.com/dubeyKartikay">
+        <Image className={styles.icon} src={GitHub} alt ="Github Icon" />
+      </a>
+      <a href="https://www.instagram.com/dubeykartikay/">
+        <Image className={styles.icon} src={Instagram} alt = "Instagram Icon" />
+      </a>
+    </div>
   )
 }
