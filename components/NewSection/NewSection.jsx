556410f27d2b5522b3f710a3e78ba798484528cd
Kartikay Dubey <kartikayd2003@gmail.com>
2023-07-24 00:59:06 +0530

    implementing paginate and search

--- components/NewSection/NewSection.jsx
+++ components/NewSection/NewSection.jsx
@@ -2,19 +2,22 @@ import connect from "../../lib/mongoDb/connect";
 import HighlightedBlog from "../HighlightedBlog/HighlightedBlog";
 import styles from "./NewSection.module.scss";
 import Link from "next/link";
+import MiniSearch from 'minisearch'
+
 export default async function NewSection({type}) {
   const res = await getBlogsMetaData(type);
   const arr = await res.map((ele) => {
+
     return <Link key={ele._id} href = {`/blog/${ele._id}`} > <HighlightedBlog  {...ele} /></Link> 
     } ).toArray();
     res.close()
-  // console.log(arr);
-  // console.log(res);
+
+  
   return (
     <div className={styles.newSection}>
       {/* <div className={styles.newSectionText} >New</div> */}
       <div className={styles.content}>
-        {arr}
+        {arr.slice(0,5)}
       </div>
     </div>
   )
@@ -22,7 +25,7 @@ export default async function NewSection({type}) {
 async function getBlogsMetaData(type) {
   const {collection} = await connect(type);
 
-  const res = collection.find().sort({ "date": -1 }).limit(4);
+  const res = collection.find().sort({ "date": -1 });
   return res;
 }
 
