48c4f1086b97c53dd27e2ffebaa552a7e729f684
Kartikay Dubey <90677227+dubeyKartikay@users.noreply.github.com>
2023-07-30 20:46:40 +0530

    mobile images fix

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -7,13 +7,13 @@ export default async function handler(req, res) {
     try {
       // this should be the actual path not a rewritten path
       // e.g. for "/blog/[slug]" this should be "/blog/post-1"
-      const path = req.body.path == null ? '/' : req.body.path;
+      const path = req.body.path == null ? '/' : req.body.path + '/1';
       await res.revalidate(path)
       return res.json({ revalidated: true })
     } catch (err) {
         console.log(err);
       // If there was an error, Next.js will continue
       // to show the last successfully generated page
-      return res.status(500).send('Error revalidating')
+      return res.status(500).send(JSON.stringify({msg : 'Error revalidating'}))
     }
   }
